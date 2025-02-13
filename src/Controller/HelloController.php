24d1fed117654bef6481ca0dd496421da2aab50b
ShahabRasool <shahabrasool12@gmail.com>
2025-02-13 15:33:20 +0500

    After installing the twig and use the method there in the twig

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -5,11 +5,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HelloController
+class HelloController extends AbstractController
 {
     #[Route('/welcome/{name}', name: "welcomeRoute ")]
     public function Welcome($name)
     {
-        return new Response("Welcome to the World of symfony mr {$name}");
+        return $this->render('hello/welcome.html.twig', ['name'=> $name]);
     }
 }
