064682f91e2ead147ce41f5496d48cb24c06e528
ShahabRasool <shahabrasool12@gmail.com>
2025-02-14 12:03:28 +0500

    used the logger service to the text in about-us.

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,8 +14,9 @@ class HelloController extends AbstractController
         return $this->render('hello/welcome.html.twig', ['name'=> $name]);
     }
     #[Route('/aboutus', name: "about-us")]
-    public function about()
+    public function about(LoggerInterface $logger)
     {
+        $logger->info('This is an info message.');
         return $this->render('hello/about.html.twig');
     }
 }
